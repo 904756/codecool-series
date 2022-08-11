4b64599deda229bce72ae01fc918bc96144a0fce
904756 <904756@gmail.com>
2022-08-11 18:09:10 +0300

    Genres in alphabetical order

--- data/queries.py
+++ data/queries.py
@@ -5,24 +5,59 @@ def get_shows():
     return data_manager.execute_select('SELECT id, title FROM shows;')
 
 
+# def get_most_rated_shows():
+#     return data_manager.execute_select('''SELECT shows.id, title, year, overview, runtime, STRING_AGG(genres.name, ', ') genres, trunc(rating, 1) rating, trailer, homepage
+#                       FROM shows
+#                       LEFT JOIN show_genres ON shows.id=show_genres.show_id
+#                       LEFT JOIN genres ON show_genres.genre_id=genres.id
+#                       GROUP BY shows.id, title, year, overview, runtime, rating, trailer, homepage
+#                       ORDER BY rating DESC
+#                       LIMIT 15''')
+
+
 def get_most_rated_shows():
-    return data_manager.execute_select('''SELECT shows.id, title, year, overview, runtime, STRING_AGG(genres.name, ', ') genres, trunc(rating, 1) rating, trailer, homepage
-                      FROM shows
-                      LEFT JOIN show_genres ON shows.id=show_genres.show_id
-                      LEFT JOIN genres ON show_genres.genre_id=genres.id
-                      GROUP BY shows.id, title, year, overview, runtime, rating, trailer, homepage
-                      ORDER BY rating DESC
-                      LIMIT 15''')
+    return data_manager.execute_select('''
+    SELECT s.id,
+    s.title,
+    STRING_AGG(DISTINCT x.name, ', ') AS genres,
+    EXTRACT(year FROM s.year) AS year,
+   s.overview,
+   s.runtime,
+   trunc(s.rating, 1) AS rating,
+   s.trailer,
+   s.homepage
+    FROM shows s
+    LEFT JOIN show_genres sg ON s.id = sg.show_id
+    LEFT JOIN genres g ON sg.genre_id = g.id
+    LEFT JOIN (
+      SELECT gg.name, ss.id
+      FROM genres gg
+      LEFT JOIN show_genres sgg ON sgg.genre_id = gg.id
+      LEFT JOIN shows ss ON sgg.show_id = ss.id
+      GROUP BY ss.id, gg.name
+      ORDER BY gg.name ASC
+    ) AS x ON x.id = s.id
+    GROUP BY s.id,
+             s.title,
+             s.year,
+             s.overview,
+             s.runtime,
+             s.rating,
+             s.trailer,
+             s.homepage
+    ORDER BY rating DESC
+    LIMIT 15;''')
 
 
 def get_show_details_by_id(show_id):
-    return data_manager.execute_select('''SELECT shows.id, title, year, overview, runtime, STRING_AGG(genres.name, ', ') genres, trunc(rating, 1) rating, trailer, homepage
+    return data_manager.execute_select('''
+    SELECT shows.id, title, year, overview, runtime, STRING_AGG(genres.name, ', ') genres, trunc(rating, 1) rating, trailer, homepage
                       FROM shows
                       LEFT JOIN show_genres ON shows.id=show_genres.show_id
                       LEFT JOIN genres ON show_genres.genre_id=genres.id
                       WHERE shows.id = %(show_id)s
                       GROUP BY shows.id, title, year, overview, runtime, rating, trailer, homepage''',
-                       {"show_id": show_id})
+                       {"show_id": show_id}, False)
 
 
 def get_list_seasons(show_id):
